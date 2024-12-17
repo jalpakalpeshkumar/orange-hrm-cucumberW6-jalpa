c1c8637c47c49544dff3cdc13fa5991c1cadb146
jimmy.shayam@gmail.com <jimmy.shayam@gmail.com>
2024-12-17 22:36:58 +0000

    My homework week 6

---
+++ src/test/java/com/orangehrmlive/demo/runner/RunCukeTest.java
@@ -0,0 +1,23 @@
+package com.orangehrmlive.demo.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+/**
+ * Created by Jay Vaghani
+ */
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/orangehrmlive/demo",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "json:target/cucumber-reports/cucumber.json"},
+        tags = "@smoke or @sanity"
+
+
+)
+public class RunCukeTest extends AbstractTestNGCucumberTests {
+
+
+}
